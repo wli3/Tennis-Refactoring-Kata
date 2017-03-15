8bd6719a8988fbf09154a2a2fbd494b59e627972
William Li <wul@microsoft.com>
2017-03-15 08:31:20 -0700

    It is stupid but it works

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,30 +1,34 @@
-using System;
+using Monad;
 
 namespace Tennis
 {
-	class TennisGame1 : ITennisGame
-	{
-		private GameState _state = GameState.GetInstance(0, 0);
+    class TennisGame1 : ITennisGame
+    {
+        private GameState _state = GameState.GetInstance(0, 0);
 
-		public void WonPoint(TennisPlayer tennisPlayer)
-		{
-			if (tennisPlayer == TennisPlayer.Player1)
-				_state.Player1Score += 1;
-			else
-				_state.Player2Score += 1;
-		}
+        public void WonPoint(TennisPlayer tennisPlayer)
+        {
+            if (tennisPlayer == TennisPlayer.Player1)
+                _state.Player1Score += 1;
+            else
+                _state.Player2Score += 1;
+        }
 
-		public string GetScore()
-		{
-			return OldEither<string, GameState>.Right(_state)
-				.Chain(TennisScoringRules.GetScoreIfEqual)
-				.Chain(TennisScoringRules.GetScoreIfDeuce)
-				.Chain(TennisScoringRules.GetScoreForNormalPlay)
-				.Chain(TennisScoringRules.GetScoreIfPlayer1Advantage)
-				.Chain(TennisScoringRules.GetScoreIfPlayer2Advantage)
-				.Chain(TennisScoringRules.GetScoreIfPlayer1Win)
-				.Chain(TennisScoringRules.GetScoreIfPlayer2Win)
-				.Left();
-		}
-	}
+        public string GetScore()
+        {
+            return
+                (from m1 in TennisScoringRules.GetScoreIfEqual(_state)
+                 from m2 in TennisScoringRules.GetScoreIfDeuce(m1)
+                 from m3 in TennisScoringRules.GetScoreForNormalPlay(m2)
+                 from m4 in TennisScoringRules.GetScoreIfPlayer1Advantage(m3)
+                 from m5 in TennisScoringRules.GetScoreIfPlayer2Advantage(m4)
+                 from m6 in TennisScoringRules.GetScoreIfPlayer1Win(m5)
+                 from m7 in TennisScoringRules.GetScoreIfPlayer2Win(m6)
+                 select m7)
+                .Match(
+                    Right: r => "Error",
+                    Left: l => l
+                )();
+        }
+    }
 }
